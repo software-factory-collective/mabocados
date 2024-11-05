ab8879a77e79d076358b00fbfd48ade728083f61
Pertempto <pertempto@github.com>
2024-11-05 18:10:59 -0500

    feat: added Multibocados component

---
+++ projects/site/src/components/games/Multibocados.vue
@@ -0,0 +1,23 @@
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+defineProps<{ level: string }>();
+
+const start = () => {
+  console.log('start!');
+}
+
+</script>
+
+<template>
+  <main class="flex flex-col items-center m-4">
+  <h1 class="text-xl">Multibocados</h1>
+
+    <button type="button" @click="start()">Start</button>
+
+  </main>
+</template>
+
+<style scoped>
+</style>
