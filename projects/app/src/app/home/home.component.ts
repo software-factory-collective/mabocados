7f54b619094be1759004c9ee6e3421eeaad7c320
Julian Campana <julian.campana@supportonics.com>
2024-11-25 18:12:37 -0300

    chore: remove angular project

--- projects/app/src/app/home/home.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'home',
-    standalone: true,
-    imports: [],
-    templateUrl: './home.component.html',
-})
-export class HomeComponent {}
